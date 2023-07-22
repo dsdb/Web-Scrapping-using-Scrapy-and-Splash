119390086b3e8fe97ebbce068ce58a0b22d61d4f
dsdb <dbsyncloud@gmail.com>
2023-07-22 10:34:50 +0530

    version 1

--- web_scrapy_proj/web_scrapy_proj/pipelines.py
+++ web_scrapy_proj/web_scrapy_proj/pipelines.py
@@ -6,22 +6,33 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import json
 # use SQLAlchemy for database storage
 from sqlalchemy import create_engine, Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-class SqllitePipeline:
+base = declarative_base()
+
+# SQLite Class
+class SqlitePipeline:
     def __init__(self):
-        engine = create_engine('sqllite://news_articles.db')
-        Base.metadata.create_all(engine)
+        engine = create_engine('sqlite:///news_articles.db')
+        base.metadata.create_all(engine)
         self.Session = sessionmaker(bind=engine)
+
+# JsonWritePipeline Class
+class JsonWriterPipeline:
+    def open_spider(self, spider):
+        self.file = open('scrapd_data.json', 'w')
+    
+    def close_spider(self, spider):
+        self.file.close()
 
 # Modify the process_item (default) method to use SQLAlchemy to create and insert the extracted data
 class WebScrapyProjPipeline:
     def process_item(self, item, spider):
-        # new insertion
+        # new insertion:SQLite
         session = self.Session()
         news_article = NewsArticle(
             title = item.get(' News title'),
@@ -31,6 +42,9 @@ class WebScrapyProjPipeline:
         session.add(news_article)
         session.commit()
         session.close()
+        # new insertion:JsonWriter
+        line = json.dump(dict(item)) + "\n"
+        self.file.write(line)
         return item
     
 
--- web_scrapy_proj/web_scrapy_proj/spiders/web_spider.py
+++ web_scrapy_proj/web_scrapy_proj/spiders/web_spider.py
@@ -3,7 +3,7 @@ from scrapy_splash import SplashRequest
 
 class WebSiteSpider(scrapy.Spider):
     name = 'onlinekhabar'
-    start_urls = ['https://english.onlinekhabar.com']
+    start_urls = ['https://english.onlinekhabar.com/content/news/']
 
 
     def start_request(self):
@@ -14,17 +14,17 @@ class WebSiteSpider(scrapy.Spider):
     def parse(self, response):
         links = response.css('#content > div > div > div > div.ok-grid-12 > div:nth-child(3) > div > a::attr(href)').extract()
         for link in links:
-            yield scrapy.Request(link.self.parse_article, dont_filter=True)
-
+            yield scrapy.Request(link, self.parse_article, dont_filter=True)
 
     def parse_article(self, response):
-        title = response.Xpath('//h2/text()').get()
-        post_id =response.url.split('/')[-2]
+        title = response.xpath('//h2/text()').get()
+        post_id = response.url.split('/')[-2]
 
-        # Extract paragraphs from the article
+    # Use the provided XPath to extract paragraphs from the article
         paragraphs = response.xpath('//*[@id="post-{}"]//div[@class="col colspan3 main__read--content ok18-single-post-content-wrap"]/p/text()'.format(post_id)).extract()
-        yield{
-            "News title":  title,
+
+        yield {
+            "News title": title,
             "Post ID": post_id,
             "Paragraphs": paragraphs,
-        }
+    }
